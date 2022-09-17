481872964bdb97071f09f715b924558a20c1b208
Paul <paul.helleu@orange.fr>
2022-09-17 18:54:28 +0200

    player_to_points

--- script.py
+++ script.py
@@ -16,3 +16,21 @@ def score_word(word):
 
 brownie_points = score_word("BROWNIE")
 print(brownie_points)
+
+player_to_words = {
+    'player1': ['BLUE', 'TENNIS', 'EXIT'],
+    'wordNerd': ['EARTH', 'EYES', 'MACHINE'],
+    'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'],
+    'Prof Reader': ['ZAP', 'COMA', 'PERIOD']
+}
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+
+
+print(player_to_points)
